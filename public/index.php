61952ddf97c4ceb65688f17b2e41453957dd140f
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2023-11-20 10:45:57 +0700

    ini contoh

--- public/index.php
+++ public/index.php
@@ -4,4 +4,6 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use TugasBesar\BookingClass2g\App\Router;
 
+// Router::add('GET', '/user', HomeController::class, 'index');
+// Router::add('GET', '/jabatan', HomeController::class, 'jabatan');
 Router::run();
