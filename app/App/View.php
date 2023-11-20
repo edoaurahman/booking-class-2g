dc2b617720f8f357da90adf107e35a0c72067293
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2023-11-20 11:31:20 +0700

    Fix path to Views directory in View.php

--- app/App/View.php
+++ app/App/View.php
@@ -7,6 +7,6 @@ class View
     public static function render(string $view, array $data): void
     {
         extract($data);
-        require __DIR__ . '/../View/' . $view . '.php';
+        require __DIR__ . '/../Views/' . $view . '.php';
     }
 }
