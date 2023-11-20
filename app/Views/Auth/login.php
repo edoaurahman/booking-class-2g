cee0c8b7369d0b49d9f174648519089a78ee07a7
langodayyy <luckykrnwn17@gmail.com>
2023-11-20 12:52:51 +0700

    tes tailwind

--- app/Views/Auth/login.php
+++ app/Views/Auth/login.php
@@ -4,12 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-    <form action="">
-        <input type="text" id="nim/nip" placeholder="NIM/NIP">
-        <input type="password" id="password" placeholder="Password">
-        <button>Login</button>
-    </form>
+    <div class="container mx-auto">
+        <form action="">
+            <input type="text" id="nim/nip" placeholder="NIM/NIP">
+            <input type="password" id="password" placeholder="Password">
+            <button>Login</button>
+        </form>
+    </div>
 </body>
 </html>
