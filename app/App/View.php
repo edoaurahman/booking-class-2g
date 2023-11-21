ab0b3cfc4408fe7b6785a317a7d98ac98d81379a
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2023-11-21 10:51:38 +0700

    add template support

--- app/App/View.php
+++ app/App/View.php
@@ -9,4 +9,15 @@ class View
         extract($data);
         require __DIR__ . '/../Views/' . $view . '.php';
     }
+
+    public static function includeTemplate(string $template): void
+    {
+        $templateFile = __DIR__ . '/../Views/templates/' . $template . '.php';
+
+        if (file_exists($templateFile)) {
+            require $templateFile;
+        } else {
+            throw new \Exception('Template file not found: ' . $template);
+        }
+    }
 }
