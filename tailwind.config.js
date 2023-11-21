11166bea6c49e40fd8e98a210fa8b2cc81aec69c
langodayyy <luckykrnwn17@gmail.com>
2023-11-21 11:50:48 +0700

    modifikasi tailwind (1)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{html,php}"],
+  content: ["./**/*.{html,php}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        navy_blue: "#003B73",
+        blue_grotto: "#60A3D9",
+        baby_blue: "#BFD7ED",
+        royal_blue: "#0074B7",
+        white: "#ffff",
+      },
+    },
   },
   plugins: [],
-}
+};
