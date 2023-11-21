0447561adf76994bf2df8daf78d801efa62f5c3a
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2023-11-21 15:46:26 +0700

    Refactor View class to include template files correctly

--- app/App/View.php
+++ app/App/View.php
@@ -7,17 +7,13 @@ class View
     public static function render(string $view, array $data): void
     {
         extract($data);
-        require __DIR__ . '/../Views/' . $view . '.php';
-    }
 
-    public static function includeTemplate(string $template): void
-    {
-        $templateFile = __DIR__ . '/../Views/templates/' . $template . '.php';
+        $templateFile =  __DIR__ . '/../Views/' . $view . '.php';
 
         if (file_exists($templateFile)) {
             require $templateFile;
         } else {
-            throw new \Exception('Template file not found: ' . $template);
+            throw new \Exception('View file not found: ' . $template);
         }
     }
 }
