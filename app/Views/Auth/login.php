72ccc132f0fd0dc6b0424287ee63aeaee6815bf4
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2023-11-23 19:56:25 +0700

    Update login.php with dark mode support

--- app/Views/Auth/login.php
+++ app/Views/Auth/login.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="light">
 
 <head>
     <meta charset="UTF-8">
@@ -276,7 +276,7 @@
                                 <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
                                 <div class="flex items-center">
                                     <input type="password" name="password" id="password" placeholder="Password" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
-                                    <i id="toggleShowPassword" class="text-white bi bi-eye-slash ml-[-30px] text-lg"></i>
+                                    <i id="toggleShowPassword" class="dark:text-white text-gray-800 bi bi-eye-slash ml-[-30px] text-lg"></i>
                                 </div>
                             </div>
                             <?php
