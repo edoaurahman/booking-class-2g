4629000242ce20ca975a4f111fc13aeae37c5cd9
MuhamadAnang <ananghusnul10@gmail.com>
2023-11-26 16:01:21 +0700

    ngutik room schedule

--- app/Views/Home/roomSchedule.php
+++ app/Views/Home/roomSchedule.php
@@ -1,64 +1,53 @@
-<section class="relative bg-[url(/assets/img/bg_home.jpg)] bg-cover bg-center bg-no-repeat h-screen w-full">
+<section class="relative bg-[url(/assets/img/bg_home.webp)] bg-cover bg-center bg-no-repeat h-screen w-full">
     <div class="absolute inset-0 bg-gradient-to-t from-white to-white-300 dark:from-dark_grey1 "></div>
 
 
 
-    <div class="relative mx-auto max-w-screen-xl pl-10 py-20 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-1">
-
-        <div class="max-w-[750px] w-auto">
-            <div class="mt-8 flex flex-wrap gap-4 text-center w-auto flex-col ">
-                <div class="flex">
-                    <button class="btn-kelas rounded-2xl block -mt-[12px]  bg-navy_blue px-5 py-3  md:text-[19px] text-[12px] text-white shadow hover:bg-bingu_hover focus:outline-none focus:ring active:bg-bingu ">
-                        Ruang Teori 5
-                    </button>
-                </div>
-                <div class="dropdown-kelas hidden flex border ">
-                    <ul class="border border-white bg-white px-5">
-                        <li><a href="">Kelas 1</a></li>
-                        <li><a href="">Kelas 1</a></li>
-                        <li><a href="">Kelas 1</a></li>
-                        <li><a href="">Kelas 1</a></li>
-                        <li><a href="">Kelas 1</a></li>
-                    </ul>
-                </div>
-                <div class='flex items-center justify-center min-h-screen fromd via-teal-300 to-teal-500 bg-gradient-to-br'>
-                    <select class="outline-none focus:outline-none p-2 bg-white rounded-3xl" value={selectedValue} onChange={handleSelectChange}>
-                        {genre.map((item, i) => (
-                        <option value={item} key={i}>
-                            Select Category
-                        </option>
-                        <option value={item} key={i}>
-                            Sci-fi
-                        </option>
-                        <option value={item} key={i}>
-                            Drama
-                        </option>
-                        <option value={item} key={i}>
-                            Thriller
-                        </option>
-                        <option value={item} key={i}>
-                            Documentary
-                        </option>
-                        ))}
+    <div class="relative mx-auto pl-10 py-20 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-1 flex justify-center items-center">
+        <div class="w-full h-full flex-1 flex justify-center items-center">
+            <div class=" w-auto">
+                <div class="mt-8 flex flex-wrap gap-4 text-center w-auto flex-col">
+                    <select id="countries" class="w-[150px] px-2 bg-blue-700  text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        <option selected>Choose a room</option>
+                        <option value="RT 1">Ruang Teori 1</option>
+                        <option value="RT 2">Ruang Teori 2</option>
+                        <option value="RT 3">Ruang Teori 3</option>
+                        <option value="RT 4">Ruang Teori 4</option>
+                        <option value="RT 5">Ruang Teori 5</option>
+                        <option value="RT 6">Ruang Teori 6</option>
+                        <option value="RT 7">Ruang Teori 7</option>
+                        <option value="RT 8">Ruang Teori 8</option>
+                        <option value="LSI 1">Lab Sistem Informasi 1</option>
+                        <option value="LSI 2">Lab Sistem Informasi 2</option>
+                        <option value="LSI 3">Lab Sistem Informasi 3</option>
+                        <option value="LPY 1">Ruang Praktikum 1</option>
+                        <option value="LPY 2">Ruang Praktikum 2</option>
+                        <option value="LPY 3">Ruang Praktikum 3</option>
+                        <option value="LPY 5">Ruang Praktikum 4</option>
+                        <option value="LPY 6">Ruang Praktikum 5</option>
+                        <option value="LPY 7">Ruang Praktikum 6</option>
+                        <option value="LPY 8">Ruang Praktikum 7</option>
                     </select>
+                </div>
+                <h1 class="text-[36px] dark:text-gray-100 font-extrabold font-sans md:text-[72px] -ml-[2px] leading-normal ">
+                    ROOM
+                    <strong class="block font-extrabold text-biru md:-mt-[37px] -mt-[12px]">
+                        SCHEDULE
+                    </strong>
+                </h1>
 
+                <p class=" max-w-lg text-[17px]/relaxed md:text-[25px] leading-normal dark:text-white">
+                    <strong>NOW</strong> TI 2G - KULIAH OOP<br>07.00 - 10.30
+                </p>
+                <div class="mt-8 flex flex-wrap gap-4 text-center w-auto  ">
+                    <a href="/booking" class="rounded-2xl block -mt-[12px]  bg-bingu px-5 py-3  md:text-[19px] text-[12px] text-white shadow hover:bg-bingu_hover focus:outline-none focus:ring active:bg-bingu ">
+                        CREATE BOOKING
+                    </a>
                 </div>
             </div>
-            <h1 class="text-[36px] dark:text-gray-100 font-extrabold font-sans md:text-[72px] -ml-[2px] leading-normal ">
-                ROOM
-                <strong class="block font-extrabold text-biru md:-mt-[37px] -mt-[12px]">
-                    SCHEDULE
-                </strong>
-            </h1>
+        </div>
+        <div class="flex-1 w-full h-full bg-">
 
-            <p class=" max-w-lg text-[17px]/relaxed md:text-[25px] leading-normal dark:text-white">
-                <strong>NOW</strong> TI 2G - KULIAH OOP<br>07.00 - 10.30
-            </p>
-            <div class="mt-8 flex flex-wrap gap-4 text-center w-auto  ">
-                <a href="/booking" class="rounded-2xl block -mt-[12px]  bg-bingu px-5 py-3  md:text-[19px] text-[12px] text-white shadow hover:bg-bingu_hover focus:outline-none focus:ring active:bg-bingu ">
-                    CREATE BOOKING
-                </a>
-            </div>
         </div>
     </div>
 </section>
