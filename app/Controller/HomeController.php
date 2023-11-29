0a8168ffbf9aad7ba9ce04b0919ebffa8478a77c
MuhamadAnang <ananghusnul10@gmail.com>
2023-11-29 08:57:58 +0700

    fix npm script dan HomeController

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -18,30 +18,78 @@ class HomeController
 
         $ruang = new Ruang();
         $totalPage = $ruang->getTotalPage();
-        $username = $_SESSION['user'];
-        $level = $_SESSION['level'];
-        if ($level == 'mahasiswa') {
-            $user = new Mahasiswa();
-            $user = $user->find($username, 'nim');
-        } else {
-            $user = new Dosen();
-            $user = $user->find($username, 'nip');
+        if (isset($_SESSION['user'])) {
+            
+            $username = $_SESSION['user'];
+            $level = $_SESSION['level'];
+            if ($level == 'mahasiswa') {
+                $user = new Mahasiswa();
+                $user = $user->find($username, 'nim');
+            } else {
+                $user = new Dosen();
+                $user = $user->find($username, 'nip');
+            }
+        }else{
+            $level = "";
+            $user = "";
         }
-        View::render("Templates/header", ['title' => 'home', 'level' => $level, 'user' => $user]);
+        View::render("Templates/header", ['title' => 'Home', 'level' => $level, 'user' => $user]);
         View::render("Home/home", ['totalPage' => $totalPage]);
         View::render("Templates/footer", []);
     }
 
     public function booking(): void
     {
-        View::render("Templates/header", ['title' => 'booking']);
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
+
+        $ruang = new Ruang();
+        $totalPage = $ruang->getTotalPage();
+        if (isset($_SESSION['user'])) {
+            
+            $username = $_SESSION['user'];
+            $level = $_SESSION['level'];
+            if ($level == 'mahasiswa') {
+                $user = new Mahasiswa();
+                $user = $user->find($username, 'nim');
+            } else {
+                $user = new Dosen();
+                $user = $user->find($username, 'nip');
+            }
+        }else{
+            $level = "";
+            $user = "";
+        }
+        View::render("Templates/header", ['title' => 'Booking', 'level' => $level, 'user' => $user]);
         View::render("Home/booking", []);
         View::render("Templates/footer", []);
     }
 
     public function roomSchedule($id): void
     {
-        View::render("Templates/header", ['title' => 'booking']);
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
+
+        $ruang = new Ruang();
+        $totalPage = $ruang->getTotalPage();
+        if (isset($_SESSION['user'])) {
+            
+            $username = $_SESSION['user'];
+            $level = $_SESSION['level'];
+            if ($level == 'mahasiswa') {
+                $user = new Mahasiswa();
+                $user = $user->find($username, 'nim');
+            } else {
+                $user = new Dosen();
+                $user = $user->find($username, 'nip');
+            }
+        }else{
+            $level = "";
+            $user = "";
+        }
+        View::render("Templates/header", ['title' => 'Room Schedule', 'level' => $level, 'user' => $user]);
         View::render("Home/roomSchedule", []);
         View::render("Templates/footer", []);
     }
