b26adb4603d7f11ee83e239262c4039f2943f57b
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2023-11-30 18:03:16 +0700

    Fix user display in header template and prevent caching in index.php

--- public/index.php
+++ public/index.php
@@ -4,6 +4,11 @@ if ($env['ENVIRONMENT'] == 'development') {
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
+    // Prevent caching
+    header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+    header('Expires: Sat, 26 Jul 1997 05:00:00 GMT');
+    header("Cache-Control: post-check=0, pre-check=0", false);
+    header("Pragma: no-cache");
 } elseif ($env['ENVIRONMENT'] == 'production') {
     ini_set('display_errors', 0);
     ini_set('display_startup_errors', 0);
