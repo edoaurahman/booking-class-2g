84cdc9221f40f15ff03ef3183b12fe6e0cb45f42
abidgymnastiar <abidgymnastiar@gmail.com>
2023-12-03 21:27:34 +0700

    input frondend details

---
+++ app/Views/Home/detail_booking.php
@@ -0,0 +1,182 @@
+<!-- Detail -->
+<div class="detail dark:bg-dark_grey1">
+    <!-- Header start -->
+    <ol class="flex items-center justify-center w-full text-sm py-36 px-10 font-medium text-center text-gray-500 dark:text-gray-400 bg-gray-500 sm:text-base">
+        <li class="flex md:w-full items-center text-blue-600 dark:text-blue-500 sm:after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700">
+            <span class="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500">
+                <i class="fa-solid fa-circle-check w-3.5 h-3.5 sm:w-4 sm:h-4 me-2.5"></i>
+                Pemesanan <span class="hidden sm:inline-flex sm:ms-2">Ruang</span>
+            </span>
+        </li>
+        <li class="flex md:w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700">
+            <span class="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500">
+                <span class="me-2">2</span>
+                Details <span class="hidden sm:inline-flex sm:ms-2">Ruang</span>
+            </span>
+        </li>
+        <li class="flex md:w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700">
+            <span class="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500">
+                <span class="me-2">2</span>
+                Pengisian <span class="hidden sm:inline-flex sm:ms-2">Form</span>
+            </span>
+        </li>
+        <li class="flex items-center">
+            <span class="me-2">3</span>
+            Confirmation
+        </li>
+    </ol>
+    <!-- Header end -->
+    <div class="container py-3 mx-auto ">
+        <div class="grid grid-cols-1 sm:grid-cols-8 sm:gap-4 sm:mb-11">
+            <!-- ###################### -->
+            <!-- Colom 1 -->
+            <!-- ###################### -->
+            <div class=" top-24 p-3 col-span-6 font-sans">
+                <div class="col-span-2 py-3">
+                    <h1 class="flex items-center  text-2xl uppercase font-extrabold text-slate-700 group-hover:text-white dark:text-white dark:group-hover:text-dark_grey5">
+                        ruang teori 1<span class="bg-blue-100 text-blue-800 text-xs font-semibold me-2 px-2.5 py-0.5 rounded dark:bg-dark_grey4 dark:text-slate-100 ms-2 dark:group-hover:bg-gray-600">Lantai 5</span>
+                    </h1>
+                </div>
+                <!-- Fasilitas start -->
+
+                <div class="flex sm:flex-wrap sm:justify-start flex-nowrap overflow-x-auto pb-3 ">
+
+                    <!-- fasilitas 1 -->
+                    <div class="fas1 flex flex-none ml-8 mt-5 items-center text-gray-500 dark:text-white">
+                        <i data-tooltip-target="icon1" class="fa-solid fa-wifi my-auto mr-2 p-3 bg-bingu hover:bg-bingu_hover text-white hover:text-white dark:bg-dark_grey5 "></i>
+                        <div class="content ">
+                            <p class="uppercase text-[10px] font-normal">Fasilitas</p>
+                            <p class="text-sm font-semibold">Wifi 40 Mbps</p>
+                        </div>
+                    </div>
+                    <!-- fasilitas 1 -->
+                    <div class="fas1 flex flex-none ml-8 mt-5 items-center text-gray-500 dark:text-white">
+                        <i data-tooltip-target="icon1" class="fa-solid fa-wifi my-auto mr-2 p-3 bg-bingu hover:bg-bingu_hover text-white hover:text-white dark:bg-dark_grey5 "></i>
+                        <div class="content ">
+                            <p class="uppercase text-[10px] font-normal">Fasilitas</p>
+                            <p class="text-sm font-semibold">Wifi 40 Mbps</p>
+                        </div>
+                    </div>
+                    <!-- fasilitas 1 -->
+                    <div class="fas1 flex flex-none ml-8 mt-5 items-center text-gray-500 dark:text-white">
+                        <i data-tooltip-target="icon1" class="fa-solid fa-wifi my-auto mr-2 p-3 bg-bingu hover:bg-bingu_hover text-white hover:text-white dark:bg-dark_grey5 "></i>
+                        <div class="content ">
+                            <p class="uppercase text-[10px] font-normal">Fasilitas</p>
+                            <p class="text-sm font-semibold">Wifi 40 Mbps</p>
+                        </div>
+                    </div>
+                    <!-- fasilitas 1 -->
+                    <div class="fas1 flex flex-none ml-8 mt-5 items-center text-gray-500 dark:text-white">
+                        <i data-tooltip-target="icon1" class="fa-solid fa-wifi my-auto mr-2 p-3 bg-bingu hover:bg-bingu_hover text-white hover:text-white dark:bg-dark_grey5 "></i>
+                        <div class="content ">
+                            <p class="uppercase text-[10px] font-normal">Fasilitas</p>
+                            <p class="text-sm font-semibold">Wifi 40 Mbps</p>
+                        </div>
+                    </div>
+                    <!-- fasilitas 1 -->
+                    <div class="fas1 flex flex-none ml-8 mt-5 items-center text-gray-500 dark:text-white">
+                        <i data-tooltip-target="icon1" class="fa-solid fa-wifi my-auto mr-2 p-3 bg-bingu hover:bg-bingu_hover text-white hover:text-white dark:bg-dark_grey5 "></i>
+                        <div class="content ">
+                            <p class="uppercase text-[10px] font-normal">Fasilitas</p>
+                            <p class="text-sm font-semibold">Wifi 40 Mbps</p>
+                        </div>
+                    </div>
+                    <!-- fasilitas 1 -->
+                    <div class="fas1 flex flex-none ml-8 mt-5 items-center text-gray-500 dark:text-white">
+                        <i data-tooltip-target="icon1" class="fa-solid fa-wifi my-auto mr-2 p-3 bg-bingu hover:bg-bingu_hover text-white hover:text-white dark:bg-dark_grey5 "></i>
+                        <div class="content ">
+                            <p class="uppercase text-[10px] font-normal">Fasilitas</p>
+                            <p class="text-sm font-semibold">Wifi 40 Mbps</p>
+                        </div>
+                    </div>
+                    <!-- fasilitas 1 -->
+                    <div class="fas1 flex flex-none ml-8 mt-5 items-center text-gray-500 dark:text-white">
+                        <i data-tooltip-target="icon1" class="fa-solid fa-wifi my-auto mr-2 p-3 bg-bingu hover:bg-bingu_hover text-white hover:text-white dark:bg-dark_grey5 "></i>
+                        <div class="content ">
+                            <p class="uppercase text-[10px] font-normal">Fasilitas</p>
+                            <p class="text-sm font-semibold">Wifi 40 Mbps</p>
+                        </div>
+                    </div>
+                    <!-- fasilitas 1 -->
+                    <div class="fas1 flex flex-none ml-8 mt-5 items-center text-gray-500 dark:text-white">
+                        <i data-tooltip-target="icon1" class="fa-solid fa-wifi my-auto mr-2 p-3 bg-bingu hover:bg-bingu_hover text-white hover:text-white dark:bg-dark_grey5 "></i>
+                        <div class="content ">
+                            <p class="uppercase text-[10px] font-normal">Fasilitas</p>
+                            <p class="text-sm font-semibold">Wifi 40 Mbps</p>
+                        </div>
+                    </div>
+                    <!-- fasilitas 1 -->
+                    <div class="fas1 flex flex-none ml-8 mt-5 items-center text-gray-500 dark:text-white">
+                        <i data-tooltip-target="icon1" class="fa-solid fa-wifi my-auto mr-2 p-3 bg-bingu hover:bg-bingu_hover text-white hover:text-white dark:bg-dark_grey5 "></i>
+                        <div class="content ">
+                            <p class="uppercase text-[10px] font-normal">Fasilitas</p>
+                            <p class="text-sm font-semibold">Wifi 40 Mbps</p>
+                        </div>
+                    </div>
+                    <!-- fasilitas 1 -->
+                    <div class="fas1 flex flex-none ml-8 mt-5 items-center text-gray-500 dark:text-white">
+                        <i data-tooltip-target="icon1" class="fa-solid fa-wifi my-auto mr-2 p-3 bg-bingu hover:bg-bingu_hover text-white hover:text-white dark:bg-dark_grey5 "></i>
+                        <div class="content ">
+                            <p class="uppercase text-[10px] font-normal">Fasilitas</p>
+                            <p class="text-sm font-semibold">Wifi 40 Mbps</p>
+                        </div>
+                    </div>
+
+                </div>
+                <!-- Fasilitas end -->
+                <!-- Description Start  -->
+                <div class="description pt-4">
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit… Zril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur.</p>
+
+                    <ul class="pl-5 pt-4">
+                        <li class="my-auto leading-7"><i class="fa-solid fa-circle-chevron-right "></i><span class="ml-3 text-sm font-normal capitalize my-auto">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</span></li>
+                        <li class="my-auto leading-7"><i class="fa-solid fa-circle-chevron-right "></i><span class="ml-3 text-sm font-normal capitalize my-auto">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</span></li>
+                        <li class="my-auto leading-7"><i class="fa-solid fa-circle-chevron-right "></i><span class="ml-3 text-sm font-normal capitalize my-auto">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</span></li>
+                    </ul>
+
+                    <p class="pt-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+                <!-- Description End  -->
+                <!-- Informasi Kelas start  -->
+                <div class="kelas mt-7">
+                    <div class="judul">
+                        <h2 class="font-bold text-xl">Informasi Kelas</h2>
+                    </div>
+                    <div class="grid grid-cols-6 gap-3 mt-5 bg-red-600">
+                        <div class="bg-sky-400">01</div>
+                        <div class="bg-sky-500">02</div>
+                        <div class="bg-sky-600">03</div>
+                        <div class="bg-sky-700">04</div>
+                        <div class="bg-sky-800">05</div>
+                        <div class="bg-sky-500">06</div>
+                        <div class="bg-sky-500">07</div>
+                        <div class="bg-sky-500">08</div>
+                        <div class="bg-sky-500">09</div>
+                        <div class="bg-sky-500">10</div>
+                        <div class="bg-sky-500">11</div>
+                    </div>
+                </div>
+                <!-- Informasi Kelas end  -->
+                <!-- Lantai start -->
+                <div class="Denah mt-7">
+                    <h2 class="font-bold text-xl">Denah Lantai</h2>
+                    <img class="object-cover rounded-md" src="/assets/img/lantai/Lantai6.png" alt="" />
+                </div>
+                <!-- Lantai end -->
+            </div>
+            <!-- ###################### -->
+            <!-- Colom 2 -->
+            <!-- ###################### -->
+            <div class=" col-span-2 font-sans bg-red-600 h-7">
+                <!-- content booking  end -->
+
+                <!-- content booking  end -->
+            </div>
+            <div class="mb-96"></div>
+        </div>
+        <!-- filter sidebar mobile -->
+    </div>
+</div>
+
+
+<script src="/assets/js/detail_booking.js"></script>
