90af6e89e58b7e329287f1351b8cc1102875a0e1
Luckykrnwn <luckykrnwn17@gmail.com>
2023-12-04 13:26:25 +0700

    menambahkan form

--- app/Views/Home/formulir-checkout.php
+++ app/Views/Home/formulir-checkout.php
@@ -1,68 +1,3 @@
-<!-- <style>
-    * {
-        box-sizing: border-box;
-    }
-
-    body {
-        font: 16px Arial;
-    }
-
-    /*the container must be positioned relative:*/
-    .autocomplete {
-        position: relative;
-        display: inline-block;
-    }
-
-    input {
-        border: 1px solid transparent;
-        background-color: #f1f1f1;
-        padding: 10px;
-        font-size: 16px;
-    }
-
-    input[type=text] {
-        background-color: #f1f1f1;
-        width: 100%;
-    }
-
-    input[type=submit] {
-        background-color: DodgerBlue;
-        color: #fff;
-        cursor: pointer;
-    }
-
-    .autocomplete-items {
-        position: absolute;
-        border: 1px solid #d4d4d4;
-        border-bottom: none;
-        border-top: none;
-        z-index: 99;
-        /*position the autocomplete items to be the same width as the container:*/
-        top: 100%;
-        left: 0;
-        right: 0;
-    }
-
-    .autocomplete-items div {
-        padding: 10px;
-        cursor: pointer;
-        background-color: #fff;
-        border-bottom: 1px solid #d4d4d4;
-    }
-
-    /*when hovering an item:*/
-    .autocomplete-items div:hover {
-        background-color: #e9e9e9;
-    }
-
-    /*when navigating through the items using the arrow keys:*/
-    .autocomplete-active {
-        background-color: DodgerBlue !important;
-        color: #ffffff;
-    }
-</style> -->
-
-
 <body>
     <div class="w-full">
         ini step
@@ -111,7 +46,7 @@
                     </div>
 
                     <div class="mb-5">
-                        <label for="dosen-pengajar" class="text-sm mb-2 font-medium text-gray-900 dark:text-white text-[15px]">Dosen pengguna ruang saat ini:</label>
+                        <label for="dosen-pengajar" class="text-sm mb-2 font-medium text-gray-900 dark:text-white text-[15px]">Dosen pengguna ruang saat ini</label>
                         <input type="text" id="dosen-pengajar" value="pak yoppy" disabled class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                     </div>
 
