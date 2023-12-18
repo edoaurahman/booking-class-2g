26ac6b5a831aba5421d2f500aff942672a69f59c
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2023-12-18 18:37:55 +0700

    Refactor modal in booking.php, change modal using alpinejs

--- app/Views/Admin/booking.php
+++ app/Views/Admin/booking.php
@@ -309,38 +309,30 @@
                                                 </a>
                                             </td>
                                             <td class="px-6 py-4">
-                                                <!-- Modal toggle -->
-                                                <span :data-modal-target="'modal-keterangan-' + index" :data-modal-toggle="'modal-keterangan-' + index" x-text="item && item.keterangan ? item.keterangan.substring(0, 20) + '...' : ''"></span>
-                                                <!-- Main modal -->
-                                                <div :id="'modal-keterangan-' + index" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-                                                    <div class="relative p-4 w-full max-w-2xl max-h-full">
-                                                        <!-- Modal content -->
-                                                        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                                                            <!-- Modal header -->
-                                                            <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-                                                                <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-                                                                    Keterangan
-                                                                </h3>
-                                                                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" :data-modal-hide="'modal-keterangan-' + index">
-                                                                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                                                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                                                <!-- ########### Ini Modal YGY ########### -->
+                                                <div x-data="{ 'showModal': false }" @keydown.escape="showModal = false">
+                                                    <!-- Trigger for Modal -->
+                                                    <button type="button" @click="showModal = true" x-text="item && item.keterangan ? item.keterangan.substring(0,20) + '...' : '' "></button>
+                                                    <!-- Modal -->
+                                                    <div class="fixed inset-0 z-30 flex items-center justify-center overflow-auto bg-black bg-opacity-50" x-show="showModal">
+                                                        <!-- Modal inner -->
+                                                        <div class="max-w-3xl px-6 py-4 mx-auto text-left bg-white rounded shadow-lg dark:bg-gray-600" @click.away="showModal = false" x-transition:enter="motion-safe:ease-out duration-300" x-transition:enter-start="opacity-0 scale-90" x-transition:enter-end="opacity-100 scale-100">
+                                                            <!-- Title / Close-->
+                                                            <div class="flex items-center justify-between">
+                                                                <h5 class="mr-3 text-black max-w-none dark:text-white">Keterangan</h5>
+                                                                <button type="button" class="z-50 cursor-pointer" @click="showModal = false">
+                                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor">
+                                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                                                                     </svg>
-                                                                    <span class="sr-only">Close modal</span>
                                                                 </button>
                                                             </div>
-                                                            <!-- Modal body -->
-                                                            <div class="p-4 md:p-5 space-y-4">
-                                                                <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400" x-text="item.keterangan">
-                                                                </p>
-                                                            </div>
-                                                            <!-- Modal footer -->
-                                                            <div class="flex justify-end items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
-                                                                <button :data-modal-hide="'modal-keterangan-' + index" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Tutup</button>
+                                                            <!-- content -->
+                                                            <div class="min-h-[100px] min-w-[300px] pt-4 max-h-[80vh] overflow-y-auto text-gray-500 dark:text-gray-300">
+                                                                <p x-text="item.keterangan"></p>
                                                             </div>
                                                         </div>
                                                     </div>
                                                 </div>
-
                                             </td>
                                             <td class="px-6 py-4 font-medium">
                                                 <template x-if="item.status == 'success' || item.status == 'done'">
@@ -546,7 +538,6 @@
         -moz-appearance: textfield;
     }
 </style>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.js"></script>
 <script src="/assets/js/sweetalert.min.js"></script>
 <script>
     // set tanggal hari ini dan maks 6 hari kedepan
