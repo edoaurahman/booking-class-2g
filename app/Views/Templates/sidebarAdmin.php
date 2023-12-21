6d6e57ef08d6c24abe35a4d13ed519b423ca6daf
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2023-12-21 09:01:26 +0700

    Update sidebarAdmin.php to fix dashboard link

--- app/Views/Templates/sidebarAdmin.php
+++ app/Views/Templates/sidebarAdmin.php
@@ -51,7 +51,7 @@
                             </div>
                             <ul class="py-1" role="none">
                                 <li>
-                                    <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Dashboard</a>
+                                    <a href="/admin" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Dashboard</a>
                                 </li>
                                 <li>
                                     <div class="border-t-[1px] border-gray-300 dark:border-gray-600">
